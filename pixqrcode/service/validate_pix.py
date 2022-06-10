7f70e5474c95d1043a8b8de892d27cc94ce1b455
Nathan Almeida <30378181+NathanAllmeida@users.noreply.github.com>
2022-06-09 22:24:26 -0300

    implement new types of key

--- pixqrcode/service/validate_pix.py
+++ pixqrcode/service/validate_pix.py
@@ -47,20 +47,85 @@ class ValidatePix:
         else:
             self.pix.reference_label = FormatValues.texts_no_space(self.pix.reference_label)
 
+    def validateCPF(self):
+        cpf = self.pix.mobile
+        cpf = re.sub('\D', '', cpf)
+
+        numbers = [int(digit) for digit in cpf if digit.isdigit()]
+        if len(numbers) != 11 or len(set(numbers)) == 1:
+            return False
+
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:9], range(10, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[9] != expected_digit:
+            return False
+
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[10] != expected_digit:
+            return False
+
+        return True
+
+    """
+        Valida o tipo de chave e retorna o codigo de acordo com o BC
+    """
+    def detect_type_key(self):
+        key = self.pix.mobile
+        regex = re.search(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z\.a-zA-Z]{1,10}$', key)
+        # 0111 - CPF
+        # 0111 - CNPJ
+        # 0125 - EMAIL
+        # 0114 - Telefone
+
+        code = ""
+        if regex is None:
+            key_numbers = re.sub('\D', '', key)
+            if len(key_numbers) == 10:
+                #valida se é um telefone fixo
+                code = '0114'
+
+            if len(key_numbers) == 12:
+                #valida se é um telefone com DDI
+                code = '0114'
+
+            if len(key_numbers) == 13:
+                #valida se é um celular com DDI
+                code = '0114'
+
+            elif self.validateCPF():
+                #valida se é um cpf com base nos calculos de digitos
+                code = '0111'
+            elif len(key_numbers) == 11:
+                #se na ultima validacao nao retornou true é bem provavel que seja celular
+
+                code = '0114'
+            elif len(key_numbers) == 14:
+                #verifica se é cnpj
+                code = '0111' #creio que o codigo esteja errado, nao achei nada sobre
+            else:
+                raise Exception("A chave informada não foi identificada")
+        else:
+            # É email
+            code = '0125'
+
+        return code
+
     def mobile(self):
         if not self.pix.mobile:
-            raise PixError("telefone nao informado")
+            raise PixError("Chave nao informado")
+        code = self.detect_type_key()
+        if code == '0114':
+            self.pix.mobile = FormatValues.mobile(self.pix.mobile)
+            if not re.match(r'^.55[\d]{3}', self.pix.mobile):
+                if not re.match(r'^55[\d]{3}', self.pix.mobile):
+                    self.pix.mobile = f"+55{self.pix.mobile}"
+                else:
+                    self.pix.mobile = f"+{self.pix.mobile}"
 
-        self.pix.mobile = FormatValues.mobile(self.pix.mobile)
-        if not re.match(r'^.55[\d]{3}', self.pix.mobile):
-            if not re.match(r'^55[\d]{3}', self.pix.mobile):
-                self.pix.mobile = f"+55{self.pix.mobile}"
-            else:
-                self.pix.mobile = f"+{self.pix.mobile}"
+            if 14 > len(self.pix.mobile) < 14:
+                raise PixError("telefone curto ou longo")
 
-        if 14 > len(self.pix.mobile) < 14:
-            raise PixError("telefone curto ou longo")
-
-        if not re.match(r'^.+55[\d]{3}', self.pix.mobile):
-            raise PixError("telefone sem o DDD")
+            if not re.match(r'^.+55[\d]{3}', self.pix.mobile):
+                raise PixError("telefone sem o DDD")
         return True
